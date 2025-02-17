541868ec9ca932194419a6b69fa43623c46a11e7
睿音 <ray@raycloud.online>
2025-02-17 12:57:53 +0800

    Update _worker.js
    更新_worker.js，修复Docker返回请求头缺失x-amz-content-sha256的问题

--- _worker.js
+++ _worker.js
@@ -517,6 +517,8 @@ function httpHandler(req, pathname, baseHost) {
 
 	const refer = reqHdrNew.get('referer');
 
+	reqHdrNew.delete("Authorization"); // 修复s3错误
+
 	let urlStr = pathname;
 
 	const urlObj = newUrl(urlStr, 'https://' + baseHost);
